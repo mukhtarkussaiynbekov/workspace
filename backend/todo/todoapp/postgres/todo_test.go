7ac6aa20293eee1e4d77c85fa65e67844022b303
Mukhtar Kussaiynbekov <67886131+mukhtarkv@users.noreply.github.com>
2024-04-22 04:03:30 +0900

    Handle errors (#6)

--- backend/todo/todoapp/postgres/todo_test.go
+++ backend/todo/todoapp/postgres/todo_test.go
@@ -60,23 +60,24 @@ func (s *todoTestSuite) TestFetch() {
 	assert.Equal(s.T(), "buy milk and bread", todo.Details)
 }
 
-// func (s *todoTestSuite) TestList() {
-// 	todo, err := s.st.List(context.Background())
-// 	assert.NoError(s.T(), err)
-// 	assert.NotNil(s.T(), todo)
-// 	assert.Equal(s.T(), 2, len(todo))
-// 	assert.Equal(s.T(), "xid2", todo[1].Id)
-// 	assert.Equal(s.T(), "visit dentist", todo[1].Title)
-// 	assert.Equal(s.T(), "get scaling", todo[1].Details)
-// }
+func (s *todoTestSuite) TestList() {
+	todo, err := s.st.List(context.Background())
+	assert.NoError(s.T(), err)
+	assert.NotNil(s.T(), todo)
+	assert.Equal(s.T(), 2, len(todo))
+	assert.Equal(s.T(), "xid2", todo[1].Id)
+	assert.Equal(s.T(), "visit dentist", todo[1].Title)
+	assert.Equal(s.T(), "get scaling", todo[1].Details)
+}
 
 func (s *todoTestSuite) TestCreate() {
 	ctx := context.Background()
-	err := s.st.Create(ctx, todoapp.ToDoItem{
+	todoItem := todoapp.ToDoItem{
 		Id: "xid3",
 		Title: "Create Test",
 		Details: "Create Test Details",
-	})
+	}
+	err := s.st.Create(ctx, todoItem)
 	assert.NoError(s.T(), err)
 	todo, err := s.st.Fetch(ctx, "xid3")
 	assert.NoError(s.T(), err)
@@ -84,6 +85,12 @@ func (s *todoTestSuite) TestCreate() {
 	assert.Equal(s.T(), "xid3", todo.Id)
 	assert.Equal(s.T(), "Create Test", todo.Title)
 	assert.Equal(s.T(), "Create Test Details", todo.Details)
+
+	// Clean up
+	err = s.st.Create(ctx, todoItem)
+	assert.NotNil(s.T(), err)
+	assert.ErrorContains(s.T(), err, "already exists")
+	s.st.Delete(ctx, "xid3")
 }
 
 func (s *todoTestSuite) TestDelete() {
@@ -94,8 +101,10 @@ func (s *todoTestSuite) TestDelete() {
 		Details: "Delete Test Details",
 	})
 	assert.NoError(s.T(), err)
+	
 	err = s.st.Delete(ctx, "xid4")
 	assert.NoError(s.T(), err)
 	_, err = s.st.Fetch(ctx, "xid4")
 	assert.NotNil(s.T(), err)
+	assert.ErrorContains(s.T(), err, "not found")
 }
